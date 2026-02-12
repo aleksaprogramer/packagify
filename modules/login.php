a37a220fd4cc3acc358615361a10e516d9fbec53
Aleksa <office.aleksa.milanovic@gmail.com>
2026-02-12 15:48:50 +0100

    Adding CSRF token

--- modules/login.php
+++ modules/login.php
@@ -1,6 +1,12 @@
 <?php
 require_once './classes/User.php';
 
+// CSRF token
+if (!isset($_SESSION['csrf-token']) || !isset($_SESSION['csrf-token-expire'])) {
+    $_SESSION['csrf-token'] = bin2hex(random_bytes(32));
+    $_SESSION['csrf-token-expire'] = time() + 600;
+}
+
 ?>
 
 <div class="login">
